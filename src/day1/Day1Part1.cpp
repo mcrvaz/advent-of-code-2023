c9e7722a8d403b71a21fb38bd4ec73b7a0c09f0e
Marcello Vaz <7875005+mcrvaz@users.noreply.github.com>
2023-12-01 13:44:02 -0300

    feat: day 1 part 1

--- src/day1/Day1Part1.cpp
+++ src/day1/Day1Part1.cpp
@@ -1,6 +1,32 @@
 #include "Day1Part1.h"
+#include <regex>
+#include "../utils/Utils.h"
+#include <iostream>
 
 int Day1Part1::solve()
 {
-	return -1;
+	const std::vector<std::string> lines = *Utils::read_lines("input/day1_input.txt").get();
+	const std::regex rgx("(\\d)");
+	std::smatch matches;
+	int sum{};
+	for (const auto& line : lines)
+	{
+		std::vector<std::string> nums{ };
+		auto lineIter = line.cbegin();
+		while (std::regex_search(lineIter, line.end(), matches, rgx))
+		{
+			nums.push_back(matches[1]);
+			lineIter = matches[0].second;
+		}
+
+		int d1 = stoi(nums.front());
+		int d2 = stoi(nums.back());
+		int num = (d1 * 10) + d2;
+		sum += num;
+	}
+
+	// answer 54239
+	std::cout << sum;
+
+	return sum;
 }
