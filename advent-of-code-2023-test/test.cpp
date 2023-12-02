6f40e61b17a7daf48bc8067acad6e5c300a90d2a
Marcello Vaz <7875005+mcrvaz@users.noreply.github.com>
2023-12-02 12:00:44 -0300

    feat: day 2 part 1

---
+++ advent-of-code-2023-test/test.cpp
@@ -0,0 +1,7 @@
+#include "pch.h"
+#include "../src/day1/Day1Part1.h"
+
+TEST(TestCaseName, TestName) {
+	const int result = Day1Part1{}.solve();
+	EXPECT_EQ(result, 54239);
+}
--- src/day2/Day2Part1.cpp
+++ src/day2/Day2Part1.cpp
@@ -1,6 +1,60 @@
 #include "Day2Part1.h"
+#include <vector>
+#include <string>
+#include "../utils/Utils.h"
+#include <regex>
+#include <unordered_map>
+#include <iostream>
+#include <algorithm>
+#include <numeric>
 
 int Day2Part1::solve()
 {
-	return -1;
+	const std::unordered_map<std::string, int> reference{
+		{ "red", 12 },
+		{ "green", 13 },
+		{ "blue", 14 }
+	};
+	const std::vector<std::string> lines = *Utils::read_lines("input/day2part1_input.txt").get();
+	const std::regex rgx("(\\d+) (red|green|blue)");
+	std::smatch matches;
+	std::vector<std::unordered_map<std::string, int>> colorCount(lines.size());
+	for (int i = 0; i < lines.size(); i++)
+	{
+		const auto& line = lines[i];
+		auto lineIter = line.cbegin();
+		while (std::regex_search(lineIter, line.end(), matches, rgx))
+		{
+			int current = colorCount[i][matches[2]];
+			colorCount[i][matches[2]] = std::max(current, stoi(matches[1].str()));
+			lineIter = matches[0].second;
+		}
+	}
+
+	std::vector<int> validGames{};
+	for (int i = 0; i < lines.size(); i++)
+	{
+		bool isValid = std::all_of(
+			reference.begin(),
+			reference.end(),
+			[&colorCount, i](std::pair<std::string, int> x)
+			{
+				return colorCount.at(i).at(x.first) <= x.second;
+			}
+		);
+
+		if (isValid)
+			validGames.push_back(i);
+	}
+
+	int result = std::reduce(
+		validGames.begin(),
+		validGames.end(),
+		0,
+		[](int acc, int gameId) { return acc + gameId + 1; }
+	);
+
+	std::cout << result;
+
+	return result;
 }
